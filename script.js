f71b0457d143b03d032e1c5f853d6119fe4c119e
Hridoy-Modak <hridoychandramodak@gmail.com>
2024-03-30 14:28:25 +0600

    Upload

---
+++ script.js
@@ -0,0 +1,30 @@
+// let x = "yes";
+// let z = "MERN-2308";
+// let y = prompt("Are you student of CIT");
+// if (y == x) {
+//   alert("Welcome Hridoy");
+//   let p = prompt("Please Type batch name(use Capital Letter)");
+//   if (p == z) {
+//     alert("you can join this class");
+//   } else {
+//     alert("you are not eligible to access this class");
+//   }
+// } else {
+//   alert("Without CIT student enterance access is not Granted");
+// }
+
+
+let x = "yes";
+let z = "MERN-2308";
+let y = prompt("Are you student of CIT");
+if (y == x) {
+  alert("Welcome Hridoy");
+  let p = prompt("Please Type batch name(use Capital Letter)");
+  if (p == z) {
+    alert("you can join this class");
+  } else {
+    alert("you are not eligible to access this class");
+  }
+} else {
+  alert("Without CIT student enterance access is not Granted");
+}
